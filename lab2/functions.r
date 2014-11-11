c13c443d622e254ce9aabae11112774d04a74cf2
char-lie <5962274+char-lie@users.noreply.github.com>
2014-11-11 20:48:31 +0200

    Divided lab2 into two files

---
+++ lab2/functions.r
@@ -0,0 +1,22 @@
+# https://github.com/earowang/statsR/blob/master/splitDf.R
+# Divide sample into test and training
+splitdf <- function(dataframe, seed=NULL) {
+    if (!is.null(seed)) set.seed(seed)
+    index <- 1:nrow(dataframe)
+    trainindex <- sample(index, trunc(length(index)/5))
+    trainset <- dataframe[trainindex, ]
+    testset <- dataframe[-trainindex, ]
+    list(trainset=trainset,testset=testset)
+}
+
+# Covariation to iris class
+cov_to_result <- function(x) {
+         if (x < -EPSILON) return('setosa')
+    else if (x >  EPSILON) return('virginica')
+    else                   return('versicolor')
+}
+
+# Compare two vectors and return the number of equalities
+calculate_correct_answers <- function(x, y) {
+    sum(as.integer(x == y))
+}
--- lab2/main.r
+++ lab2/main.r
@@ -1,51 +1,42 @@
 library(monmlp)
+source("functions.r")
 
+# http://stackoverflow.com/a/1173161/1305036
 wideScreen()
 
-# https://github.com/earowang/statsR/blob/master/splitDf.R
-splitdf <- function(dataframe, seed=NULL) {
-    if (!is.null(seed)) set.seed(seed)
-    index <- 1:nrow(dataframe)
-    trainindex <- sample(index, trunc(length(index)/5))
-    trainset <- dataframe[trainindex, ]
-    testset <- dataframe[-trainindex, ]
-    list(trainset=trainset,testset=testset)
-}
-
-BORDER  <- 10
+# Classification constants
+BORDER  <- 100
 EPSILON <- 2*BORDER/3
 
+# Convert classes names to numbers
 dc <- rep(1, nrow(iris))
 dc[iris$Species == 'virginica']  <-  BORDER
 dc[iris$Species == 'setosa']     <- -BORDER
 dc[iris$Species == 'versicolor'] <- 0
-
+# Append numbers to iris dataset
 sample <- cbind(iris, dc)
 
-dim(sample)
-colnames(sample)
+# Divide sample into training and test sample for each class separately
+# to avoid irregularity
 samples <- mapply(splitdf,
     Map(function(x) sample[sample$Species == x,], unique(sample[,"Species"])))
-
+# Concatenate training samples
 training_sample <- Reduce(function(x,y) {rbind(x,y)}, samples["trainset",])
+# Concatenate test samples
 test_sample <- Reduce(function(x,y) {rbind(x,y)}, samples["testset",])
 
+# Train the network
 ft <- monmlp.fit(x=as.matrix(training_sample[1:4]), y=as.matrix(training_sample[6]), hidden1=3)
+# Try to predict
 pt <- monmlp.predict(x=as.matrix(test_sample[1:4]), weights = ft)
 
-cov_to_result <- function(x) {
-         if (x < -EPSILON) return('setosa')
-    else if (x >  EPSILON) return('virginica')
-    else                   return('versicolor')
-}
-calculate_correct <- function(x, y) {
-    sum(as.integer(x == y))
-}
-
-
+# Translate numbers to classes
 result <- mapply(cov_to_result, pt)
+# Concatenate source and fit data
 cmp_result <- cbind(test_sample, Result=result, Cov=round(pt,1))
-correct <- calculate_correct(cmp_result$Species, cmp_result$Result)
+# Calculate correct answers
+correct <- calculate_correct_answers(cmp_result$Species, cmp_result$Result)
+# Store the length of test sample
 total <- nrow(cmp_result)
 
 print("Here is comparison matrix")
@@ -54,4 +45,5 @@ cat("You've got", correct,
     "corrrect answers of", total,
     "which is", round(100*correct/total,2),
     "%\n")
+# Log the correctness to stderr
 write(round(100*correct/total,2), stderr())
