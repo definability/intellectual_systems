32bcfaa56323ce761b1d6d2dc4ec588436ce133a
char-lie <5962274+char-lie@users.noreply.github.com>
2014-11-11 20:32:40 +0200

    Shorten the code

--- lab2/main.r
+++ lab2/main.r
@@ -1,8 +1,8 @@
 library(monmlp)
 
-#options(width=150)
 wideScreen()
 
+# https://github.com/earowang/statsR/blob/master/splitDf.R
 splitdf <- function(dataframe, seed=NULL) {
     if (!is.null(seed)) set.seed(seed)
     index <- 1:nrow(dataframe)
@@ -12,23 +12,23 @@ splitdf <- function(dataframe, seed=NULL) {
     list(trainset=trainset,testset=testset)
 }
 
-dc <- rep(1, nrow(iris))
 BORDER  <- 10
 EPSILON <- 2*BORDER/3
+
+dc <- rep(1, nrow(iris))
 dc[iris$Species == 'virginica']  <-  BORDER
 dc[iris$Species == 'setosa']     <- -BORDER
 dc[iris$Species == 'versicolor'] <- 0
 
 sample <- cbind(iris, dc)
 
-samples <- cbind(head(sample, 50), tail(head(sample,100), 50), tail(sample, 50))
+dim(sample)
+colnames(sample)
+samples <- mapply(splitdf,
+    Map(function(x) sample[sample$Species == x,], unique(sample[,"Species"])))
 
-sample1 <- splitdf(samples[1:6])
-sample2 <- splitdf(samples[7:12])
-sample3 <- splitdf(samples[13:18])
-
-training_sample <- rbind(sample1$trainset, sample2$trainset, sample3$trainset)#splits$trainset
-test_sample <- rbind(sample1$testset, sample2$testset, sample3$testset)#splits$testset
+training_sample <- Reduce(function(x,y) {rbind(x,y)}, samples["trainset",])
+test_sample <- Reduce(function(x,y) {rbind(x,y)}, samples["testset",])
 
 ft <- monmlp.fit(x=as.matrix(training_sample[1:4]), y=as.matrix(training_sample[6]), hidden1=3)
 pt <- monmlp.predict(x=as.matrix(test_sample[1:4]), weights = ft)
@@ -43,15 +43,15 @@ calculate_correct <- function(x, y) {
 }
 
 
-result <- sapply(pt, cov_to_result)
-cmp_result <- cbind(test_sample, result, as.integer(pt))
-cmp_result
-#intersect(cmp_result$Species, cmp_result$result)
-#all(cmp_result$Species == cmp_result$result)
-correct <- calculate_correct(cmp_result$Species, cmp_result$result)
+result <- mapply(cov_to_result, pt)
+cmp_result <- cbind(test_sample, Result=result, Cov=round(pt,1))
+correct <- calculate_correct(cmp_result$Species, cmp_result$Result)
 total <- nrow(cmp_result)
 
+print("Here is comparison matrix")
+cmp_result
 cat("You've got", correct,
     "corrrect answers of", total,
-    "which is", as.integer(10000*correct/total)/100,
+    "which is", round(100*correct/total,2),
     "%\n")
+write(round(100*correct/total,2), stderr())
